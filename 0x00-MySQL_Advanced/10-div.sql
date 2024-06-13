039b9bf81f987c63ff4c4d8b8d0b610152bfe54c
DevBasem <engr.basemahmed@gmail.com>
2024-06-13 15:12:45 +0000

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,13 @@
 -- Create the SafeDiv function
-DELIMITER $$
+DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10,4)
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (
+a INT,
+b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
-    END IF;
-END$$
+    RETURN (IF (b = 0, 0, a / b));
+END //
 
 DELIMITER ;
