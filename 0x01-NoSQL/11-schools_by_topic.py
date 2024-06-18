54764ec226639d5bd71b31d31a2e16e01497055f
DevBasem <engr.basemahmed@gmail.com>
2024-06-18 14:55:02 -0700

    task 11 fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,6 +8,4 @@ def schools_by_topic(mongo_collection, topic):
     Retrieve a list of schools that have a specific topic.
     """
     # Query documents where 'topics' contains the specified topic
-    schools = list(mongo_collection.find({'topics': topic}))
-
-    return schools
+    return mongo_collection.find({"topics": topic})
